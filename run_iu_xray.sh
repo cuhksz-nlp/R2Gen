ba830e50d44b7286fe9d335ce3a77b54d5d78780
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-03-26 13:21:03 +0100

    add back the codes

---
+++ run_iu_xray.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xe
+
+export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/versions/miniconda3-latest/bin:$HOME/
+.local/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+
+pyenv local miniconda3-latest
+
+cd "$HOME/gputest"
+
+python main.py \
+--image_dir data/iu_xray/images/ \
+--ann_path data/iu_xray/annotation.json \
+--dataset_name iu_xray \
+--max_seq_length 60 \
+--threshold 3 \
+--batch_size 16 \
+--epochs 100 \
+--save_dir results/iu_xray \
+--step_size 50 \
+--gamma 0.1 \
+--seed 9223
+
