6d998abcf8871e0dbc048a5c357ad821d8d9f3f1
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-06-12 17:21:23 +0200

    get matched data

--- data_processors/data_processors.py
+++ data_processors/data_processors.py
@@ -12,10 +12,10 @@ class DataProcessor(object):
         self.kaggle_iu_reports = csv.reader(open(self.kaggle_iu_reports_path, 'r'))
         self.kaggle_iu_reports_header = next(self.kaggle_iu_reports)
 
-    def associate_iu_r2gen_kaggle(self):
+    def associate_iu_r2gen_kaggle_by_id(self):
         r2gen_splits_ids_reports = {
-            r2gen_split: [{sample["id"]: sample["report"]} for sample in samples]
-            for r2gen_split, samples in self.r2gen_ann.items()
+            split: [{sample["id"]: sample["report"]} for sample in samples]
+            for split, samples in self.r2gen_ann.items()
         }
 
         kaggle_uids_mesh_impression = {
@@ -23,4 +23,23 @@ class DataProcessor(object):
             for line in self.kaggle_iu_reports
         }
 
-
+        unmatched = dict(train=[], val=[], test=[])
+        matched = dict(train=[], val=[], test=[])
+        for split, samples in r2gen_splits_ids_reports.items():
+            for sample in samples:
+                for r2gen_id, r2gen_report in sample.items():
+                    uid = r2gen_id.split('_')[0].replace("CXR", "")
+                    if uid in kaggle_uids_mesh_impression:
+                        kaggle_report = kaggle_uids_mesh_impression[uid]["report"]
+                        if r2gen_report == kaggle_report:
+                            matched_info = {r2gen_id: {"MeSH": kaggle_uids_mesh_impression[uid]["MeSH"],
+                                                       "impression": kaggle_uids_mesh_impression[uid]["impression"]}}
+                            matched[split].append(matched_info)
+                        else:
+                            unmatched_info = {r2gen_id: {"r2gen_uid": uid, "r2gen_report": r2gen_report,
+                                                         "kaggle_report": kaggle_report}}
+                            unmatched[split].append(unmatched_info)
+                    else:
+                        unmatched_info = {
+                            r2gen_id: {"r2gen_uid": uid, "r2gen_report": r2gen_report, "kaggle_report": ""}}
+                        unmatched[split].append(unmatched_info)
