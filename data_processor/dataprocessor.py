0e9899d2879672488bc44f161385b9648adde4d1
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-06-12 14:47:33 +0200

    dataprocessor test

--- data_processor/dataprocessor.py
+++ data_processor/dataprocessor.py
@@ -1,15 +1,17 @@
 import json
+import csv
 
 
 class DataProcessor:
     def __int__(self, args):
         self.r2gen_ann_path = args.ann_path
         self.r2gen_ann = json.loads(open(self.ann_path, 'r').read())
-        self.kaggle_iu_projections_path = args.kaggle_iu_projections_path
-        self.kaggle_iu_projections = json.loads(open(self.kaggle_iu_projections_path, 'r').read())
+        # self.kaggle_iu_projections_path = args.kaggle_iu_projections_path
+        # self.kaggle_iu_projections = json.loads(open(self.kaggle_iu_projections_path, 'r').read())
         self.kaggle_iu_reports_path = args.kaggle_iu_reports_path
-        self.kaggle_iu_reports = json.loads(open(self.kaggle_iu_reports_path, 'r').read())
+        self.kaggle_iu_reports = csv.reader(self.kaggle_iu_reports_path)
 
     def associateIuxrayR2genToKaggle(self):
-        r2gen_splits_ids = {r2gen_split: [v["id"]] for r2gen_splits, values in self.r2gen_ann.items() for r2gen_split, v
-                            in values}
+        r2gen_splits_ids = {r2gen_split: {v["id"]: v["report"]} for r2gen_splits, values in self.r2gen_ann.items() for
+                            r2gen_split, v in values}
+        print(self.kaggle_iu_reports[0])
