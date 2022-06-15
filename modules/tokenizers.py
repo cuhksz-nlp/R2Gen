9c989adeb16f4f3f8186af00f4d43ab2b2d0da1e
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-06-15 12:06:40 +0200

    remove annotations from the generated result

--- modules/tokenizers.py
+++ modules/tokenizers.py
@@ -103,7 +103,7 @@ class Tokenizer(object):
                     # exp setup
                     # remove ontology annotization
                     tkn = self.idx2token[idx]
-                    if ':' not in tkn:
+                    if '<mesh:' not in tkn or '<attr:' not in tkn or '<sep>' not in tkn:
                         txt += tkn
                     #################################
             else:
