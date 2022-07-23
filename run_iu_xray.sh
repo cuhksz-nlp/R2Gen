1c371e17cc9c05953015ae0f1c91e712cdf00a04
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-07-23 09:47:01 +0200

    --exp 3 --max_seq_length 130

--- run_iu_xray.sh
+++ run_iu_xray.sh
@@ -14,10 +14,10 @@ time python main.py \
 --image_dir ../data/iu_xray/r2gen/images/ \
 --ann_path ../data/iu_xray/r2gen/annotation.json \
 --dataset_name iu_xray \
---max_seq_length 100 \
+--max_seq_length 130 \
 --threshold 3 \
 --batch_size 16 \
---epochs 130 \
+--epochs 100 \
 --save_dir ../r2gen_results/iu_xray \
 --step_size 50 \
 --gamma 0.1 \
