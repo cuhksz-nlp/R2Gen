c963adf675452b90beda6da0c4b5632e9e21aa2f
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-08-19 12:08:49 +0200

    adding is_new_random_split to the result name

--- modules/trainer.py
+++ modules/trainer.py
@@ -111,9 +111,10 @@ class BaseTrainer(object):
             os.makedirs(self.args.record_dir)
         # exp setup
         record_path = os.path.join(
-            self.args.record_dir, "{}-exp_{}_max_seq_length_{}_is_print_{}_remove_annotation_{}-.csv".format(
+            self.args.record_dir,
+            "{}-exp_{}_max_seq_length_{}_is_print_{}_remove_annotation_{}_is_new_split_{}-.csv".format(
                 self.args.dataset_name, self.args.exp, self.args.max_seq_length, self.args.is_print,
-                self.args.remove_annotation))
+                self.args.remove_annotation, self.args.is_new_split))
         ########################################################################################################
         if not os.path.exists(record_path):
             record_table = pd.DataFrame()
