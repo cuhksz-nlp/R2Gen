6255cb3e78857ed398764559fc743ba42dc2b5e4
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-09-08 15:08:15 +0200

    negation detection

--- analytics_main.py
+++ analytics_main.py
@@ -31,53 +31,53 @@ def main():
     negation_detection = NegationDetection(args)
     for split, split_sample in data_processor.iu_mesh_impression_split.items():
         for kid, sample in split_sample.items():
-            negation_detection.print_negation(negation_detection.get_lemmatize_doc_object(sample["report"]))
-    exp_stats = ExperimentsStatistics(tokenizer, args.exp)
-    print("exp: ", args.exp, " ", exp_stats.stats)
-
-    print("######### before split#########")
-    data_processor.analyze.print_normal_percentage()
-    data_processor.analyze.print_no_index_percentage()
-    data_processor.analyze.print_empty_mesh_asc_percentage()
-    print("Is association file valid: ", data_processor.validate_association())
-
-    plot = Plot(args, data_processor.analyze)
-    # normal ratio
-    plot.plot_stacked_bar(num=1, xs=["t_ratio", "normal_ratio"], ys=["split"],
-                          colors=[plot.abnormal_color, plot.normal_color], labels=['Abnormal', 'Normal'],
-                          number_of_col_in_legend=2, plot_name="[R2gen Split]Normal to Abnormal ratio")
-    # indexed ratio
-    plot.plot_stacked_bar(num=2, xs=["t_ratio", "no_index_ratio"], ys=["split"],
-                          colors=[plot.indexed_color, plot.no_index_color], labels=['Indexed', 'No Indexing'],
-                          number_of_col_in_legend=2, plot_name="[R2gen Split]Indexed to No Indexing ratio")
-    # empty mesh ratio
-    plot.plot_stacked_bar(num=3, xs=["t_ratio", "no_mesh_ratio"], ys=["split"],
-                          colors=[plot.mesh_color, plot.no_mesh_color], labels=['MeSH', 'No MeSH'],
-                          number_of_col_in_legend=2, plot_name="[R2gen Split]Mesh to No MeSH ratio")
-
-    # new split
-    args.is_new_random_split = 1
-    data_processor = DataProcessor(args)
-    print("######### after split#########")
-    data_processor.analyze.print_normal_percentage()
-    data_processor.analyze.print_no_index_percentage()
-    data_processor.analyze.print_empty_mesh_asc_percentage()
-    print("Is association file valid: ", data_processor.validate_association())
-
-    plot = Plot(args, data_processor.analyze)
-    # normal ratio
-    plot.plot_stacked_bar(num=4, xs=["t_ratio", "normal_ratio"], ys=["split"],
-                          colors=[plot.abnormal_color, plot.normal_color], labels=['Abnormal', 'Normal'],
-                          number_of_col_in_legend=2, plot_name="[New Split]Normal to Abnormal ratio")
-    # indexed ratio
-    plot.plot_stacked_bar(num=5, xs=["t_ratio", "no_index_ratio"], ys=["split"],
-                          colors=[plot.indexed_color, plot.no_index_color], labels=['Indexed', 'No Indexing'],
-                          number_of_col_in_legend=2, plot_name="[New Split]Indexed to No Indexing ratio")
-    # empty mesh ratio
-    plot.plot_stacked_bar(num=6, xs=["t_ratio", "no_mesh_ratio"], ys=["split"],
-                          colors=[plot.mesh_color, plot.no_mesh_color], labels=['MeSH', 'No MeSH'],
-                          number_of_col_in_legend=2, plot_name="[New Split]MeSH to No MeSH ratio")
-    plt.show()
+            negation_detection.get_lemmatize_doc_object(sample)
+    # exp_stats = ExperimentsStatistics(tokenizer, args.exp)
+    # print("exp: ", args.exp, " ", exp_stats.stats)
+    #
+    # print("######### before split#########")
+    # data_processor.analyze.print_normal_percentage()
+    # data_processor.analyze.print_no_index_percentage()
+    # data_processor.analyze.print_empty_mesh_asc_percentage()
+    # print("Is association file valid: ", data_processor.validate_association())
+    #
+    # plot = Plot(args, data_processor.analyze)
+    # # normal ratio
+    # plot.plot_stacked_bar(num=1, xs=["t_ratio", "normal_ratio"], ys=["split"],
+    #                       colors=[plot.abnormal_color, plot.normal_color], labels=['Abnormal', 'Normal'],
+    #                       number_of_col_in_legend=2, plot_name="[R2gen Split]Normal to Abnormal ratio")
+    # # indexed ratio
+    # plot.plot_stacked_bar(num=2, xs=["t_ratio", "no_index_ratio"], ys=["split"],
+    #                       colors=[plot.indexed_color, plot.no_index_color], labels=['Indexed', 'No Indexing'],
+    #                       number_of_col_in_legend=2, plot_name="[R2gen Split]Indexed to No Indexing ratio")
+    # # empty mesh ratio
+    # plot.plot_stacked_bar(num=3, xs=["t_ratio", "no_mesh_ratio"], ys=["split"],
+    #                       colors=[plot.mesh_color, plot.no_mesh_color], labels=['MeSH', 'No MeSH'],
+    #                       number_of_col_in_legend=2, plot_name="[R2gen Split]Mesh to No MeSH ratio")
+    #
+    # # new split
+    # args.is_new_random_split = 1
+    # data_processor = DataProcessor(args)
+    # print("######### after split#########")
+    # data_processor.analyze.print_normal_percentage()
+    # data_processor.analyze.print_no_index_percentage()
+    # data_processor.analyze.print_empty_mesh_asc_percentage()
+    # print("Is association file valid: ", data_processor.validate_association())
+    #
+    # plot = Plot(args, data_processor.analyze)
+    # # normal ratio
+    # plot.plot_stacked_bar(num=4, xs=["t_ratio", "normal_ratio"], ys=["split"],
+    #                       colors=[plot.abnormal_color, plot.normal_color], labels=['Abnormal', 'Normal'],
+    #                       number_of_col_in_legend=2, plot_name="[New Split]Normal to Abnormal ratio")
+    # # indexed ratio
+    # plot.plot_stacked_bar(num=5, xs=["t_ratio", "no_index_ratio"], ys=["split"],
+    #                       colors=[plot.indexed_color, plot.no_index_color], labels=['Indexed', 'No Indexing'],
+    #                       number_of_col_in_legend=2, plot_name="[New Split]Indexed to No Indexing ratio")
+    # # empty mesh ratio
+    # plot.plot_stacked_bar(num=6, xs=["t_ratio", "no_mesh_ratio"], ys=["split"],
+    #                       colors=[plot.mesh_color, plot.no_mesh_color], labels=['MeSH', 'No MeSH'],
+    #                       number_of_col_in_legend=2, plot_name="[New Split]MeSH to No MeSH ratio")
+    # plt.show()
     timer.time_executed(start_time, "R2Gen.Analysis")
 
 
--- data_processors/negation_detection.py
+++ data_processors/negation_detection.py
@@ -1,19 +1,29 @@
 # exp setup
 import spacy
 from negspacy.termsets import termset
+from negspacy.negation import Negex
 
 
 class NegationDetection(object):
     def __init__(self, args):
         self.args = args
         self.nlp0 = spacy.load("en_core_sci_sm")
+        self.termset = termset("en_clinical")
         self.nlp1 = spacy.load("en_ner_bc5cdr_md")
-        self.entities = ["DISEASE", "ORGAN", "NEG_ENTITY"]
+        # self.entities = ["DISEASE", "TEST", "TREATMENT", "NEG_ENTITY"]
+        self.nlp0.add_pipe("negex", config={"ent_types": ["DISEASE", "TEST", "TREATMENT", "ORGAN", "NEG_ENTITY"]})
+        self.nlp1.add_pipe("negex", config={"ent_types": ["DISEASE", "TEST", "TREATMENT", "ORGAN", "NEG_ENTITY"]})
 
     def get_lemmatize_doc_object(self, report):
-        termset("en_clinical").add_patterns({"pseudo_negations": ["normal", "stable"],})
-        lem_report = self.lemmatize(report, self.nlp0)
-        return self.nlp1(lem_report)
+        self.termset.add_patterns({
+            "pseudo_negations": ["normal", "stable"],
+            # "preceding_negations": ["normal", "stable"],
+            # "following_negations": ["normal", "stable"],
+        })
+        # print(self.termset.get_patterns())
+        for sentence in report["report"].split('.'):
+            lem_sentence = self.lemmatize(sentence, self.nlp0)
+            self.print_negation(self.nlp1(lem_sentence))
 
     def lemmatize(self, report, nlp):
         doc = nlp(report)
