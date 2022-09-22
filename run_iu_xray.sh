027d5705761968bcbbfef1735af769f9d8fb4882
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-09-22 15:04:14 +0200

    new --exp 2 --max_seq_length 100 --create_r2gen_kaggle_association 0 --is_new_random_split 0 --is_print 1 --remove_annotation 0

--- run_iu_xray.sh
+++ run_iu_xray.sh
@@ -23,7 +23,7 @@ time python main.py \
 --save_dir ../r2gen_results/iu_xray \
 --iu_mesh_impression_path data/iu_xray/kaggle/iu_mesh_impression.json \
 --is_print 1 \
---remove_annotation 1 \
+--remove_annotation 0 \
 --train_sample 0 \
 --val_sample 0 \
 --test_sample 0 \
