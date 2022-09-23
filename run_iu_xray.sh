258b883c44e8b78505fc10b3b2a274f14243a7e2
tmandal <tmandal@techfak.uni-bielefeld.de>
2022-09-23 17:52:29 +0200

    result new --exp 4 --max_seq_length 10 --create_r2gen_kaggle_association 0 --is_new_random_split 0 --is_print 1 --remove_annotation 1

--- run_iu_xray.sh
+++ run_iu_xray.sh
@@ -28,7 +28,8 @@ time python main.py \
 --val_sample 0 \
 --test_sample 0 \
 --create_r2gen_kaggle_association 0 \
---is_new_random_split 0 \
+--is_new_random_split 1 \
+--resume ../r2gen_results/iu_xray/iu_xray-exp_2_max_seq_length_100_is_print_0_remove_annotation_1_is_new_random_split_0-stdout_1597_0-model_best \
 --val_test_partial_data 0 \
 --max_seq_length 90 \
 --exp 2
